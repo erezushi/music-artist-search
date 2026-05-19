d61dd24caaee0c6809642125f2dfa95bf0a5ac06
PokéErez <erezushi900@gmail.com>
2026-05-19 13:31:41 +0300

    simplified Search Results tree

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -23,25 +23,27 @@ const SearchResults = (props: ISearchResultProps) => {
   if (!data) return <div>No data</div>;
 
   return (
-    <div>
-      {data.artists?.map((artist) => (
-        <div className="artist-result">
-          <Link to="/artist/$artistId" params={{ artistId: artist.idArtist }}>
-            {artist.strArtistThumb ? (
-              <>
-                <span>{artist.strArtist}</span>
-                <img
-                  src={artist.strArtistThumb}
-                  alt={`Thumbnail art for ${artist.strArtist}`}
-                />
-              </>
-            ) : (
-              `No Thumbnail art for ${artist.strArtist}`
-            )}
-          </Link>
-        </div>
-      )) ?? 'No artist found'}
-    </div>
+    data.artists?.map((artist) => (
+      <>
+        <Link
+          to="/artist/$artistId"
+          params={{ artistId: artist.idArtist }}
+          className="artist-result"
+        >
+          {artist.strArtistThumb ? (
+            <>
+              <span>{artist.strArtist}</span>
+              <img
+                src={artist.strArtistThumb}
+                alt={`Thumbnail art for ${artist.strArtist}`}
+              />
+            </>
+          ) : (
+            `No Thumbnail art for ${artist.strArtist}`
+          )}
+        </Link>
+      </>
+    )) ?? 'No artist found'
   );
 };
 
